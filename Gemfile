93e34c1969557d9cddbd92620685d043e189b4a6
Joshua Miller <jsmiller@uchicago.edu>
2022-10-14 16:56:46 -0500

    add docker compose with multipl redis servers

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
+source 'https://rubygems.org'
+
 gem 'redis', '~> 4.3.1'
--- redis_tributary.rb
+++ redis_tributary.rb
@@ -1,22 +1,29 @@
 require 'socket'
 require 'redis'
+require 'optparse'
 
 require_relative './proxy_connection'
 require_relative './vector_clock'
 require_relative './redis_command_buffer'
 
 class RedisProxyServer
-  def initialize(server_host: '127.0.0.1', server_port: 36379)
+  def initialize(
+    tributary_args:,
+    server_host: '0.0.0.0',
+    server_port: 36379
+  )
+    @tributary_args = tributary_args
     @server_host = server_host
     @server_port = server_port
     @server = TCPServer.new(server_host, server_port)
   end
 
   def listen
-    puts "Listening on localhost:#{@server_port}..."
+    puts "Listening on #{@server_host}:#{@server_port}..."
     loop do
       Thread.start(@server.accept) do |client|
-        redis_client = RedisTributary.new
+        puts 'Bound!'
+        redis_client = RedisTributary.new(@tributary_args)
         connection = ProxyConnection.new(client)
         connection.proxy_to_downstream(redis_client.upstream)
         connection.readlines do |line|
@@ -28,11 +35,16 @@ class RedisProxyServer
 end
 
 class RedisTributary
-  def initialize(redis_host: '127.0.0.1', redis_port: 6379)
+  def initialize(
+    node_count:,
+    node_id:,
+    redis_host: '0.0.0.0',
+    redis_port: 6379
+  )
     @redis_host = redis_host
     @redis_port = redis_port
     @redis = Redis.new(host: redis_host, port: redis_port)
-    @clock = VectorClock.new(1, 3)
+    @clock = VectorClock.new(node_id.to_i, node_count.to_i)
     @buffer = RedisCommandBuffer.new
   end
 
@@ -54,5 +66,29 @@ class RedisTributary
   end
 end
 
-server = RedisProxyServer.new
+$stdout.sync = true
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: [options]'
+
+  opts.on('-hHOST', '--redis-host=HOST', String, 'redis-host') do |v|
+    options[:redis_host] = v
+  end
+
+  opts.on('-pPORTS', '--redis-port=PORT', Integer, 'redis-port') do |v|
+    options[:redis_port] = v
+  end
+
+  opts.on('-iID', '--node-id=ID', 'node-id') do |v|
+    options[:node_id] = v
+  end
+
+  opts.on('-nCOUNT', '--node-count=COUNT', Integer, 'node-count') do |v|
+    options[:node_count] = v
+  end
+end.parse!
+
+puts 'Options:', options
+server = RedisProxyServer.new(tributary_args: options)
 server.listen
